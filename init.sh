a1ef348e71cc0cc63bfeec5a45f1201a4cdc9b31
Jason Morgan <jmorgan@f9vs.com>
2018-02-12 15:26:33 -0700

    Using case so it's a bit easier to update. Also catching bad input and quitting.

--- init.sh
+++ init.sh
@@ -31,12 +31,18 @@ bosh_repo=$(yaml2json < ./vars.local.yml | jq -r '.bosh_repo_path')
 VSPHERE_OPS="-o $bosh_repo/vsphere/cpi.yml"
 VBOX_OPS="-o $bosh_repo/virtualbox/cpi.yml -o $bosh_repo/virtualbox/outbound-network.yml -o $bosh_repo/bosh-lite.yml -o $bosh_repo/bosh-lite-runc.yml"
 
-if [ "$target_type" = "vsphere" ]; then
-  ops=$VSPHERE_OPS
-fi
-if [ "$target_type" = "vbox" ]; then
-  ops=$VBOX_OPS
-fi 
+case "$target_type" in
+  vsphere)
+    ops=$VSPHERE_OPS
+    ;;
+  vbox)
+    ops=$VBOX_OPS
+    ;;
+  *)
+    echo "target type: $target_type, is not supported."
+    echo "exiting without taking any action"
+    exit 1
+esac
 
 # Use the ops in here
 bosh "$action" "$bosh_repo/bosh.yml" \
